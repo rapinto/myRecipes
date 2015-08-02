17579c42fbaec1cd89aca7057b256d48f4175722
Ades <ades@MacBook-Pro-de-Abarth.local>
2015-08-02 12:02:36 +0200

    first commit

---
+++ MyRecipesBook/Class/NewRecipe/NewRecipeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewRecipeViewController.h
+//  MyRecipesBook
+//
+//  Created by Ades on 02/08/2015.
+//  Copyright (c) 2015 Ades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewRecipeViewController : UIViewController
+
+@end
