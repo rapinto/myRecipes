ab2c054527bfec9e0701bba0c73d8cf38b32bad4
Ades <ades@MacBook-Pro-de-Abarth.local>
2015-08-02 09:57:54 +0200

    Initial Commit

---
+++ MyRecipesBook/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  MyRecipesBook
+//
+//  Created by Ades on 02/08/2015.
+//  Copyright (c) 2015 Ades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
