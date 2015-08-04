4c208ffa7ebe83a1c7a71f7bec8d945bc50e82e3
Ades <ades@MacBook-Pro-de-Abarth.local>
2015-08-05 00:02:15 +0200

    improve textView / textField

--- MyRecipesBook/Class/NewRecipe/NewRecipeViewController.m
+++ MyRecipesBook/Class/NewRecipe/NewRecipeViewController.m
@@ -6,6 +6,13 @@
 //  Copyright (c) 2015 Ades. All rights reserved.
 //
 
+
+
+#define kTextViewMarginTop 138
+#define kTextViewMarginBottom 27
+
+
+
 #import "NewRecipeViewController.h"
 
 @interface NewRecipeViewController ()
@@ -72,6 +79,14 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    
+    [self setupView];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
 }
 
 
@@ -115,13 +130,12 @@
 
 - (void)keyboardWillHide:(NSNotification*)notification
 {
-    CGRect keyboardBounds = [[notification.userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
     NSNumber *duration = [notification.userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey];
     NSNumber *curve = [notification.userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey];
     
     [self updateTableViewFrameWithAnimationDuration:duration
                                      animationCurve:curve
-                                     keyboardHeight:keyboardBounds.size.height];
+                                     keyboardHeight:0];
 }
 
 
@@ -129,6 +143,39 @@
 #pragma mark -
 #pragma mark View Update Methods
 
+
+
+- (void)setupView
+{
+    UIView* lEmptyFooter = [[UIView alloc] initWithFrame:CGRectZero];
+    _tableView.tableFooterView = lEmptyFooter;
+    
+    
+    [self updateTableHeaderViewFrame];
+}
+
+
+- (void)updateTableHeaderViewFrame
+{
+    CGSize sizeThatFitsTextView = [_recipeDescriptiontextView sizeThatFits:CGSizeMake(_recipeDescriptiontextView.frame.size.width, MAXFLOAT)];
+    
+    
+    CGFloat lTextViewHeight = MAX(sizeThatFitsTextView.height, _tableView.frame.size.height - (kTextViewMarginTop + sizeThatFitsTextView.height + kTextViewMarginBottom));
+    
+    
+    _recipeDescriptiontextView.frame = CGRectMake(_recipeDescriptiontextView.frame.origin.x,
+                                                  _recipeDescriptiontextView.frame.origin.y,
+                                                  _recipeDescriptiontextView.frame.size.width,
+                                                  lTextViewHeight);
+    
+    _tableView.tableHeaderView.frame = CGRectMake(0,
+                                                  0,
+                                                  self.view.frame.size.width,
+                                                  kTextViewMarginTop + lTextViewHeight + kTextViewMarginBottom);
+    
+    _tableView.tableHeaderView = _tableView.tableHeaderView;
+    
+}
 
 
 - (void)updateTableViewFrameWitheyboardHeight:(float)keyboardHeight
@@ -144,7 +191,6 @@
                                    animationCurve:(NSNumber*)animationCurve
                                     keyboardHeight:(float)keyboardHeight
 {
-        // animations settings
         [UIView beginAnimations:nil context:NULL];
         [UIView setAnimationBeginsFromCurrentState:YES];
         [UIView setAnimationDuration:[animationDuration doubleValue]];
@@ -155,9 +201,15 @@
                                       _tableView.frame.origin.y,
                                       _tableView.frame.size.width,
                                       self.view.frame.size.height - keyboardHeight);
-        
-        // commit animations
         [UIView commitAnimations];
+    
+    
+    NSLog(@"self frame %@", [NSValue valueWithCGRect:self.view.frame]);
+    
+    NSLog(@"next frame %@", [NSValue valueWithCGRect:CGRectMake(_tableView.frame.origin.x,
+                                                               _tableView.frame.origin.y,
+                                                               _tableView.frame.size.width,
+                                                               self.view.frame.size.height - keyboardHeight)]);
 }
 
 
@@ -209,6 +261,13 @@
     
     return YES;
 }
+
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    [self updateTableHeaderViewFrame];
+}
+
 
 
 @end
